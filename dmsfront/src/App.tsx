e1ddb0ad7413bf58379309b6601a90df2b0b1889
Jamie Tait <taitjamie@outlook.com>
2025-04-30 12:57:50 -0700

    "login page created and ready for backend"

--- dmsfront/src/App.tsx
+++ dmsfront/src/App.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './App.css';
 import Home from './components/home/home.tsx'
 import Report from './components/report/report.tsx'
-import Profile from './components/profile/profile.tsx'
+import Login from './components/login/login.tsx'
 
 function App() {
   // const [count, setCount] = useState(0);
@@ -15,7 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/report" element={<Report />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
